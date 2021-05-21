d854d66ba22096af1a4e3161ee1d1cdf74fe26c9
acul71 <luca.pisani@birdo.net>
2021-05-21 02:31:44 +0200

    Add react index file

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//import {tradeOgreInit, checkPasswordFile} from './lib/utils'
+
+//tradeOgreInit()
+//const firstTime = checkPasswordFile() ? false : true
+const firstTime = false
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App firstTime={firstTime}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
