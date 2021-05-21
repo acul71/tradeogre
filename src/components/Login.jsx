c5b1ba90db542691b3fa1a7ac75426d37a7093e8
acul71 <luca.pisani@birdo.net>
2021-05-21 02:16:00 +0200

    Add Login component. Need password to login (just a proof of concept for now)

---
+++ src/components/Login.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+//import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+const bcrypt = require('bcryptjs')
+
+const PASSWORD = 'test'
+const salt = bcrypt.genSaltSync(12)
+const passwordHash = bcrypt.hashSync(PASSWORD, salt)
+
+function Login(props) {
+   let password = React.createRef()
+   let passwordMsg = React.createRef()
+
+   function handleSubmit() {
+      const passToCheck = password.current.value
+      if ( bcrypt.compareSync(passToCheck, passwordHash) ) {
+      //if (password.current.value === PASSWORD ) {
+         passwordMsg.current.textContent = ''
+         console.log('Password is right!')
+         props.enableAuth()
+      } else {
+         passwordMsg.current.textContent = 'Wrong password!'
+      }
+
+   }
+   
+
+   return (
+      <Container fluid="sm">
+         <Form onSubmit={(e) => e.preventDefault()}>
+            <Form.Group className='ml-1 mt-3' controlId="password">
+               <Col sm={3}>
+                  <Form.Label className="text-center">Password</Form.Label>
+               </Col>
+               <Col sm={4}>
+                  <Form.Control type="password" placeholder="Insert password" ref={password} />
+               </Col>
+               <Col sm={4}>
+               <p className="text-danger" ref={passwordMsg}></p>
+               </Col>
+            </Form.Group>
+            <Col sm={3}>
+               <Button variant="primary" type="submit" onClick={handleSubmit}>Submit</Button>
+            </Col>
+         </Form>
+      </Container>
+   )
+}
+
+export default Login
