08a5d4a09a3f5206fe189a61510f06b54306c1c5
acul71 <luca.pisani@birdo.net>
2021-12-13 09:28:49 +0100

    Add wallet_example json wallet

---
+++ src/wallet_example.js
@@ -0,0 +1,31 @@
+const walletData = [
+  {
+    key: "tradeogre_ARRR",
+    exchangeId: "tradeogre",
+    ticker: "ARRR",
+    name: "Pirate Chain",
+    balance: 1000,
+    price: 0,
+    exchangeCoinInfo: {}
+  },
+  {
+    key: "tradeogre_XMR",
+    exchangeId: "tradeogre",
+    ticker: "XMR",
+    name: "Monero",
+    balance: 10,
+    price: 0,
+    exchangeCoinInfo: {}
+  },
+  {
+    key: "tradeogre_BTC",
+    exchangeId: "tradeogre",
+    ticker: "XUSD",
+    name: "Bitcoin",
+    balance: 0.1,
+    price: 0,
+    exchangeCoinInfo: {}
+  },
+]
+
+export { walletData }
